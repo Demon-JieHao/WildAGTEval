7d941f3865676a29c72c20416e76dc2c799ed03a
Doyoung <doyo94550754@gmail.com>
2025-12-27 12:48:04 +0900

    clean comments

--- API_Complexity/scripts/run_cumulative_eval.sh
+++ API_Complexity/scripts/run_cumulative_eval.sh
@@ -70,6 +70,9 @@ get_prompts_for_uncertainty() {
 set_env_dir() {
     local env=$1
     case "$env" in
+        "Combined_transformed_toy")
+            ENV_DIR="Combined_transformed_toy"
+            ;;
         "Combined_transformed")
             ENV_DIR="Combined_transformed"
             ;;
